4214c8f879d0ae77e068813d011957065006129b
hungbt <hac.unq@gmail.com>
2018-12-12 13:31:45 +0700

    LoginActivity
    Signed-off-by: hungbt <hac.unq@gmail.com>

--- Lotus_Coffee/app/src/main/java/com/example/lotuscoffeeapp/Admin.java
+++ Lotus_Coffee/app/src/main/java/com/example/lotuscoffeeapp/Admin.java
@@ -3,7 +3,7 @@ package com.example.lotuscoffeeapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class Admin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
