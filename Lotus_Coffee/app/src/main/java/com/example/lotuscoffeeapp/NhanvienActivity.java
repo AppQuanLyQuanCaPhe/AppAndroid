a58412ddee09601e3c3d2277cc875a7683bc724a
hungbt <hac.unq@gmail.com>
2018-12-17 23:47:32 +0700

    fix
    Signed-off-by: hungbt <hac.unq@gmail.com>

--- Lotus_Coffee/app/src/main/java/com/example/lotuscoffeeapp/NhanvienActivity.java
+++ Lotus_Coffee/app/src/main/java/com/example/lotuscoffeeapp/NhanvienActivity.java
@@ -11,18 +11,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NhanvienActivity extends AppCompatActivity {
-//
-//    SQLiteDatabase database;
-//    NhanVienApdapter adapter;
-//    ArrayList<NhanVien> NhanVienList;
-//    ListView lvNhanVien;
 
     SQLiteDatabase database;
     NhanVienApdapter adapter;
     List<NhanVien> NhanVienList;
+    ListView lvNhanVien;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nhanvien);
+        database=Database.initDatabase(NhanvienActivity.this,MainActivity.DATABASE_NAME);
+        lvNhanVien=(ListView) findViewById(R.id.ListViewNhanVien);
+        NhanVienList=new ArrayList<>();
+        GetDataNhanVien();
+        adapter=new NhanVienApdapter(NhanvienActivity.this,android.R.layout.simple_list_item_1,NhanVienList);
+        lvNhanVien.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+    private void GetDataNhanVien(){
+        Cursor cursor=database.rawQuery("SELECT * FROM NhanVien",null);
+
+        while (cursor.moveToNext()){
+            NhanVien td=new NhanVien();
+            td.setMaNV(cursor.getInt(0));
+            td.setHoTen(cursor.getString(1));
+            td.setSDT(cursor.getString(2));
+            td.setNgaySinh(cursor.getString(3));
+            td.setEmail(cursor.getString(4));
+            td.setDiaChi(cursor.getString(5));
+            NhanVienList.add(td);
+        }
     }
 }
